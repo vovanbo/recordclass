ce0d174a65b9c7466a96d76ed9e44626cbd217e5
Vladimir Bolshakov <vovanbo@gmail.com>
2017-03-29 17:10:51 +0300

    Move benchmark to tests

--- tests/benchmark.py
+++ tests/benchmark.py
@@ -1,40 +1,39 @@
-
-# coding: utf-8
-
-# In[ ]:
-
+#!/usr/bin/env python
 from collections import namedtuple
+from timeit import timeit
 
 from trafaretrecord import trafaretrecord
 
 STest = namedtuple("TEST", "a b c")
-sa = STest(a=1,b=2,c=3)
+sa = STest(a=1, b=2, c=3)
 
 RTest = trafaretrecord("RTEST", "a b c")
-ra = RTest(a=1,b=2,c=3)
+ra = RTest(a=1, b=2, c=3)
+
 
 class Test(object):
-    __slots__ = ["a","b","c"]
+    __slots__ = ("a", "b", "c")
 
     def __init__(self, a, b, c):
-        self.a = a; self.b = b; self.c = c
+        self.a = a
+        self.b = b
+        self.c = c
 
-b = Test(1,2,3)
 
-c = {'a':1, 'b':2, 'c':3}
+b = Test(1, 2, 3)
 
-d = (1,2,3)
-e = [1,2,3]
-f = (1,2,3)
-g = [1,2,3]
+c = {'a': 1, 'b': 2, 'c': 3}
+
+d = (1, 2, 3)
+e = [1, 2, 3]
+f = (1, 2, 3)
+g = [1, 2, 3]
 key = 2
-
-from timeit import timeit
 
 print("Named tuple with a, b, c:")
 print(timeit("z = sa.c", "from __main__ import sa"))
 
-print("Recordclass with a, b, c:")
+print("TrafaretRecord with a, b, c:")
 print(timeit("z = ra.c", "from __main__ import ra"))
 
 print("Named tuple, using index:")
@@ -57,4 +56,3 @@ print(timeit("z = d[key]", "from __main__ import d, key"))
 
 print("List with three values, using a local key:")
 print(timeit("z = e[key]", "from __main__ import e, key"))
-
