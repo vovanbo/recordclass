833607b2930c043a80ea297a49854b00c3ef9050
zaur shibzukhov <szport@gmail.com>
2017-03-31 10:53:41 +0300

    Make source for typing support compatible with python < 3.5

--- lib/recordclass/test/test_typing.py
+++ lib/recordclass/test/test_typing.py
@@ -1,34 +1,32 @@
 """Unit tests for recordclass.py."""
-import typing
 import unittest, doctest, operator
 from recordclass import recordclass
-#from collections import OrderedDict
-#import pickle, copy
-#import keyword
-#import re
-#import sys
-
-from recordclass import RecordClass
 
 try:
     from test import support
 except:
     from test import test_support as support
 
+import sys as _sys
+_PY36 = _sys.version_info[:2] >= (3, 6)
 
-class RecordClassTypingTest(unittest.TestCase):
-    def test_typing(self):
-        class A(RecordClass):
-            __annotations__ = {'a':int, 'b':str, 'c':typing.List[int]}
-            #a: int
-            #b: str
-            #c: typing.List[int]
+if _PY36:
+    import typing
+    from recordclass import RecordClass
 
-        tmp = A(a=1, b='1', c=[1, 2, 3])
-        self.assertEqual(repr(tmp), "A(a=1, b='1', c=[1, 2, 3])")
-        self.assertEqual(tmp._field_types, {'a': int, 'b': str, 'c': typing.List[int]})
+    class RecordClassTypingTest(unittest.TestCase):
+        def test_typing(self):
+            class A(RecordClass):
+                __annotations__ = {'a': int, 'b': str, 'c': typing.List[int]}
 
-def main():
-    suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(RecordClassTypingTest))
-    return suite
+            tmp = A(a=1, b='1', c=[1, 2, 3])
+            self.assertEqual(repr(tmp), "A(a=1, b='1', c=[1, 2, 3])")
+            self.assertEqual(tmp._field_types, {'a': int, 'b': str, 'c': typing.List[int]})
+
+    def main():
+        suite = unittest.TestSuite()
+        suite.addTest(unittest.makeSuite(RecordClassTypingTest))
+        return suite
+else:
+    def main():
+        pass
